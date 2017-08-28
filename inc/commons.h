738f48a19d61b9864056ac91b3956f18173dafeb
Federico Di Pierro <nierro92@gmail.com>
2017-08-28 21:45:58 +0200

    UUID needs only 36 bytes.

--- inc/commons.h
+++ inc/commons.h
@@ -10,11 +10,13 @@
 #include <libudev.h>
 #include <sys/inotify.h>
 
+#define UUID_LEN 36
+
 struct trash_dir {
     char trash_path[PATH_MAX + 1];          // path to trash dir
     char info_path[PATH_MAX + 1];           // subdir of trash dir
     char files_path[PATH_MAX + 1];          // subdir of trash dir
-    char uuid[PATH_MAX + 1];                // root dir for this filesystem
+    char uuid[UUID_LEN + 1];                // UUID for this trash FS
     int inot_wd;                            // inotify watcher
     int num_trashed;                        // number of trashed files in this trash
 };
